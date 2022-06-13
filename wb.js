63866666a513841b467023ee4185e0413a48683d
lukedever <29476413+lukedever@users.noreply.github.com>
2022-06-13 15:30:43 +0800

    Update wb.js

--- wb.js
+++ wb.js
@@ -9,7 +9,7 @@ function filter_timeline_cards(cards) {
         while (j--) {
             let item = cards[j];
             if (item.data) {
-              if (item.data.timestamp_text && gg_words.includes(item.data.timestamp_text)) {
+              if (item.data.mblogtypename && gg_words.includes(item.data.mblogtypename)) {
                 console.log(item.data.timestamp_text, j)
                 cards.splice(j, 1);
               }
