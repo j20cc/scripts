f617c56137b1a29b618af29b0b09d0b312f17b50
lukedever <29476413+lukedever@users.noreply.github.com>
2022-10-26 13:21:09 +0800

    Create bilibili.js

---
+++ bilibili.js
@@ -0,0 +1,22 @@
+//https://app.bilibili.com/x/v2/feed/index
+const url = $request.url;
+let body = $response.body;
+
+function filter_feed_items(items) {
+    if (items && items.length > 0) {
+        let j = items.length;
+        while (j--) {
+            let item = items[j];
+            if (item.data.ad_info) {
+                items.splice(j, 1);
+            }
+            
+        }
+    }
+    return items;
+}
+
+let obj = JSON.parse(body);
+if (obj.data.items) obj.data.items = filter_feed_items(obj.data.items);
+body = JSON.stringify(obj);
+$done({ body });
