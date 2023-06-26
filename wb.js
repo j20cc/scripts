20b94466ccaad094305f66de45a3f685b7d6503f
j20cc <29476413+j20cc@users.noreply.github.com>
2023-06-26 18:46:16 +0800

    Update wb.js

--- wb.js
+++ wb.js
@@ -1,4 +1,4 @@
-//https://api.weibo.cn/2/statuses/container_timeline
+//https://api.weibo.cn/2/statuses/*
 const url = $request.url;
 let body = $response.body;
 
@@ -10,7 +10,6 @@ function filter_timeline_cards(cards) {
             let item = cards[j];
             if (item.data) {
               if (item.data.mblogtypename && gg_words.includes(item.data.mblogtypename)) {
-                console.log(item.data.timestamp_text, j)
                 cards.splice(j, 1);
               }
             }
@@ -20,7 +19,23 @@ function filter_timeline_cards(cards) {
     return cards;
 }
 
+function filter_head_cards(cards) {
+    if (cards.length > 0) {
+        let j = cards.length;
+        while (j--) {
+            let item = cards[j];
+            if (item.actionlog) {
+              if (item.actionlog.source == 'ad') {
+                cards.splice(j, 1);
+              }
+            }
+        }
+    }
+    return cards;
+}
+
 let obj = JSON.parse(body);
-if (obj.items) obj.items = filter_timeline_cards(obj.items);
+if (obj.items && url.includes('container_timeline')) obj.items = filter_timeline_cards(obj.items);
+if (obj.head_cards && url.includes('extend')) obj.head_cards = filter_head_cards(obj.head_cards);
 body = JSON.stringify(obj);
 $done({ body });
